5bba4d6b6a14ace7327f4baf1436438552268883
Moshood Adeleye <adeleyemosh@gmail.com>
2023-01-02 17:38:23 +0100

    Added postgresql database

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'food132&Carry&&',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
