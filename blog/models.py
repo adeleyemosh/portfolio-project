602d9c6a3213876411f1e5a566070be865777fc8
Moshood Adeleye <adeleyemosh@gmail.com>
2023-01-03 00:13:58 +0100

    Polished the website, collected static and much more

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField(blank=False)
     image = models.ImageField(upload_to="blog_images/")
+
+    def __str__(self) -> str:
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_small(self):
+        return self.pub_date.strftime("%b %e %Y")
